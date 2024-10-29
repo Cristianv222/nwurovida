cde3c2ead7b1997f4a4c9f7594afa937c73d3318
Cris20034 <cris.vasq2003@hotmail.com>
2024-10-28 19:11:29 -0500

    Update topbar.php

--- admin_panel/includes/topbar.php
+++ admin_panel/includes/topbar.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ConfiguroWeb | Dashboard</title>
+    <title>NeuroVida | Dashboard</title>
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen">

ff1d30e2bc90a15a35e1abd6b82796080053aaa3
Cris20034 <cris.vasq2003@hotmail.com>
2024-10-28 22:56:56 -0500

    Update acompañamientoDoulas.php

---
+++ neuro/Servicios_individuales/acompañamientoDoulas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acompañamiento de doulas</title>
+    <link rel="shortcut icon" href="/neuro/images/apple-icon-144x144.png" type="image/x-icon">
+    <link rel="stylesheet" href="/neuro/css/estilo.css">
+</head>
+<body>
+<header class="header">
+        <div class="menu container">
+            <nav class="navbar">
+                <a href="index.php" class="logo"><img src="/neuro/images/logotipo.png" alt=""></a>
+                <ul>
+                    <li><a href="./index.php">Inicio</a></li>
+                    <li><a href="quienes_somos.php">¿Quienes somos?</a></li>
+                    <li><a href="./contactanos.php">Contactanos</a></li>
+                    <li><a href="servicios.php">Servicios</a></li>
+                </ul>
+                <div>
+                    <!-- IMPORTANTE, se puede cambiar por botones, pero  el href se debe hacer referencia al mismo -->
+                    <a href="./admin_panel/find-result.php" class="btn-estud">Estudiantes</a>
+                    <a href="./admin_panel/admin-login.php" class="btn-Adm">Maestros</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
+
+<style>
+
+</style>
+
+<script>
+
+</script>

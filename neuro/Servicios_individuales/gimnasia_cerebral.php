c835adf248204ae76bb8a84f3d2db5d17c1c1661
YYORCH <theyorch.music@gmail.com>
2024-10-29 22:11:24 -0500

    Creacion del servicio

---
+++ neuro/Servicios_individuales/gimnasia_cerebral.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gimnasia Cerebral</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/neuro/css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="menu container">
+            <a href="#" class="logo">NeuroVida</a>
+              <input type="checkbox" id="menu" /> 
+              <label for="menu">
+              <img src="../images/menu.png" class="menu-icono" alt="menu">
+
+              </label>
+              <nav class="navbar">
+                <ul>
+                    <li><a href="./index.php">Inicio</a></li>
+                    <li><a href="quienes_somos.php">¿Quienes somos?</a></li>
+                    <li><a href="./contactanos.php">Contactanos</a></li>
+                    <li><a href="servicios.php">Servicios</a></li>
+    
+                </ul>
+              </nav>
+        </div>
+        <div class="header-content container">
+            <div class="header-txt">
+                <h1>Gimnasia Cerebral</h1>
+                <p>
+                Descubre ejercicios mentales que mejoran tu memoria, concentración y creatividad. 
+                ¡Fortalece tu mente y transforma tu día a día!
+                </p>
+                <a href="#" class="btn-1">Informacion</a>
+            </div>
+            <div class="header-img">
+                <img src="../images/left.png" alt="">
+
+            </div>
+
+        </div>
+
+
+
+    </header>
+
+    <section class="about container">
+        <div class="about-img">
+            <img src="../images/gs4.jpg" alt="">
+        </div>
+        <div class="about-txt">
+            <h2>Niños de cualquier edad/Adultos mayores</h2>
+            <p>
+            Ejercicios Mentales: A través de ejercicios mentales diseñados 
+            para mejorar la memoria, la atención y el razonamiento, mantener 
+            su mente activa y saludable, ayudando a prevenir el deterioro 
+            cognitivo. 
+            </p>
+
+            <p>
+            Mejora las habilidades cognitivas, sociales y físicas mediante 
+            ejercicios que activan ambos hemisferios del cerebro, mejorando no 
+            solo el aprendizaje sino también el bienestar emocional.  
+            </p>
+
+        </div>
+
+    </section>
+
+    <main class="servicios">
+        <h2>Beneficios</h2>
+        <div class="servicios-content container">
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-hospital-user"></i>
+                <h3>Coordinación Motora Fina y Gruesa </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-stethoscope"></i>
+                <h3>Concentración y Memoria  </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-solid fa-brain"></i>
+                <h3>Creatividad y Resolución de Problemas   </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-solid fa-hospital"></i>
+                <h3>Lateralidad y Equilibrio </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-solid fa-brain"></i>
+                <h3>Atención y Focalización  </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-stethoscope"></i>
+                <h3>Coordinación Ojo-Mano   </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-book"></i>
+                <h3>Trabajo en Equipo y Comunicación    </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-solid fa-brain"></i>
+                <h3>Razonamiento Lógico  </h3>
+            </div>
+
+            <div class="servicio-1">
+                <i class="fa-sharp fa-solid fa-book"></i>
+                <h3>Ejercicios Cruzados  </h3>
+            </div>
+
+
+
+        </div>
+
+    </main>
+
+    <section class="pricing">
+        <div class="plan">
+        <h3>Plan Diario</h3>
+            <p class="price"><span class="discount">Antes: $1</span> $1.50/días</p>
+            <p class="benefit">Acceso por 7 días</p>
+            <ul class="benefits">
+            <li>✔ Flexibilidad: Acceso según la disponibilidad diaria.</li>
+            <li>✔ Costo Asequible: Ideal para probar el servicio.</li>
+            <li>✔ Opción de Adaptación: Práctica ajustable diariamente.</li>
+            <li>✔ Ejercicios Breves: Actividades cortas y efectivas.</li>
+
+            </ul>
+            <button>Elegir plan</button>
+        </div>
+        <div class="plan popular">
+            <h3>Plan Semanal</h3>
+            <p class="price"><span class="discount">Antes: $10</span> $7/semana</p>
+            <p class="benefit">Acceso por 7 días</p>
+            <ul class="benefits">
+            <li>✔ Compromiso Moderado: Enfoque a corto plazo.</li>
+            <li>✔ Pruebas Rápidas: Evaluación intensiva en una semana.</li>
+            <li>✔ Variedad de Ejercicios: Sesiones frescas y motivadoras.</li>
+            <li>✔ Conexión Social: Participación en sesiones grupales.</li>
+
+            </ul>
+            <button>Elegir plan</button>
+        </div>
+        <div class="plan">
+            <h3>Plan Mensual</h3>
+            <p class="price"><span class="discount">Antes: $40</span> $30/mes</p>
+            <p class="benefit">Acceso por 30 días</p>
+            <ul class="benefits">
+            <li>✔ Acceso Ilimitado: Máximo aprovechamiento de sesiones.</li>
+            <li>✔ Progreso Sostenido: Mejoras en memoria y concentración.</li>
+            <li>✔ Recursos Exclusivos: Guías y ejercicios adicionales.</li>
+            <li>✔ Evaluación Mensual: Medición de progreso mensual.</li>
+
+            </ul>
+            <button>Elegir plan</button>
+        </div>
+    </section>
+
+      
+
+    
+
+</body>
+</html>

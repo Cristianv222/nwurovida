c3a6c531e8bb00195747fdd624147c4b9dbe0c50
Cris20034 <cris.vasq2003@hotmail.com>
2024-11-06 05:52:40 -0500

    cambios

--- quienres_somos.php
+++ quienres_somos.php
@@ -3,12 +3,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/neuro/css/somos.css">
 </head>
 <body>
-<?php include './includes/menu_nav.php'; ?>
+    <header class="hero">
+    <?php include './includes/menu_nav.php'; ?>
+    <div class="hero-content container">
+            <div class="hero-txt">
 
-<?php include './includes/menu_nav.php'; ?>
-</body>
+            <h1>¿QUIENES SOMOS?</h1>
+                <p>
+                
+                </p>
+                <a href="#" class="btn_somos">Agenda tu cita</a>
+            </div>
+        </div>
+
+    </header>
+
 <?php include './includes/footer.php'; ?>
-
+</body>
 </html>

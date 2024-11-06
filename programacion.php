3e7ff1bca650da4fc9ad0e5cf9064c3ee330913f
Cris20034 <cris.vasq2003@hotmail.com>
2024-11-05 21:32:58 -0500

    cambios

---
+++ programacion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NeuroVida-Programacion</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <?php include '../nwurovida/includes/menu_nav.php'; ?>
+    <header class="hero">
+        <H1>
+            Curso de programacion
+        </H1>
+        <a href="">Inscribete Ya </a>
+    </header>
+
+    <section>
+        <div class="card">
+
+        </div>
+    </section>
+    <?php include '../nwurovida/includes/footer.php'; ?>
+</body>
+</html>

fc7a1eeda24f41d8e5d20dae43f7f2e7bbd5a824
YYORCH <theyorch.music@gmail.com>
2024-11-05 22:00:12 -0500

    dfdfs

---
+++ neuro/Servicios_individuales/acompañamientoDoulas.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Acompañamiento Doulas</title>
+    <link rel="shortcut icon" href="./neuro/images/apple-icon-144x144.png" type="image/x-icon">
+    <link rel="stylesheet" href="./neuro/css/doulas.css">
+</head>
+<body>
+<header class="header_doulas">
+    <!-- Remover o actualizar la siguiente línea si 'menu_nav.php' está en otra ubicación -->
+    <?php include '../nwurovida/includes/menu_nav.php'; ?>
+    <div class="header-content container">
+        <div class="header-txt">
+            <h1>ACOMPAÑAMIENTO DE LAS DOULAS</h1>
+            <p></p>
+            <a href="#" class="btn-1">Informacion</a>
+        </div>
+        <div class="header-img">
+            <img src="../neuro/images/doula1.png" alt="">
+        </div>
+    </div>
+</header>
+<section class="about container">
+    <div class="about-img">
+        <img src="../images/doula2.png" alt="">
+    </div>
+    <div class="about-txt">
+        <h2>Mujeres en estado de gestación</h2>
+        <p>
+            Es fundamental para el bienestar de las mujeres en gestación, 
+            especialmente aquellas que enfrentan desafíos emocionales, 
+            físicos o sociales. Mejora las experiencias durante el embarazo, 
+            sino que también facilitaría la creación de vínculos afectivos positivos con sus hijos, 
+            fortaleciendo la salud mental de ambos y ayudando a mitigar los efectos del estrés cognitivo.
+        </p>
+    </div>
+</section>
+
+<main class="servicios">
+    <h2>Beneficios</h2>
+    <div class="servicios-content container">
+        <div class="servicio-1">
+            <i class="fa-solid fa-brain"></i>
+            <h3>Apoyo emocional y físico</h3>
+            <p>
+                Las Doulas proporcionan una presencia constante durante el embarazo,
+                el parto y el posparto, lo que genera una sensación de seguridad 
+                y tranquilidad. Su apoyo ayuda a reducir los niveles de estrés y ansiedad, 
+                lo que es particularmente importante para las mujeres que viven situaciones difíciles.
+            </p>
+        </div>
+        <div class="servicio-1">
+            <i class="fa-sharp fa-solid fa-stethoscope"></i>
+            <h3>Empoderamiento</h3>
+            <p>
+                Las doulas promueven el empoderamiento de la mujer durante 
+                su proceso de gestación, dándole herramientas para tomar decisiones
+                informadas sobre su cuerpo y su parto. Este acompañamiento ayuda a 
+                que la madre se sienta más en control de su experiencia, lo que tiene un impacto 
+                positivo en su bienestar mental y emocional.
+            </p>
+        </div>
+        <div class="servicio-1">
+            <i class="fa-sharp fa-solid fa-book"></i>
+            <h3>Beneficios para la salud</h3>
+            <p>
+                Estudios han mostrado que las mujeres que cuentan con el apoyo de una doula tienen menos 
+                probabilidades de requerir intervenciones médicas durante el 
+                parto, como cesáreas, y es más probable que experimenten un 
+                parto más corto y menos doloroso. Estos beneficios se traducen 
+                en una recuperación más rápida y menos complicaciones en el posparto.
+            </p>
+        </div>
+        <div class="servicio-1">
+            <i class="fa-solid fa-brain"></i>
+            <h3>Acompañamiento continuo</h3>
+            <p>
+                Las doulas también juegan un papel crucial en el posparto, 
+                brindando apoyo en el cuidado del recién nacido, la lactancia y la adaptación a la maternidad.
+                Esto es vital para las mujeres que se encuentran en condiciones de vulnerabilidad o estrés,
+                ya que contribuye a la creación de un entorno más estable y amoroso para el bebé y la madre.
+            </p>
+        </div>
+    </div>
+</main>
+
+<section class="pricing">
+    <div class="plan">
+        <h3>Plan Diario</h3>
+        <p class="price"><span class="discount"></span> $1.33/Día</p>
+        <p class="benefit">Acceso por 1 día</p>
+        <ul class="benefits">
+            <li>Flexibilidad: Acceso según la disponibilidad diaria.</li>
+            <li>Costo Asequible: Ideal para probar el servicio por un día.</li>
+            <li>Adaptación Rápida: Práctica ajustable diariamente.</li>
+            <li>Apoyo Emocional Inmediato: Sesiones breves de acompañamiento emocional y físico.</li>
+            <li>Herramientas de Empoderamiento: Ejercicios para fortalecer la autoconfianza y la toma de decisiones.</li>
+        </ul>
+        <button>Elegir plan</button>
+    </div>
+    <div class="plan popular">
+        <h3>Plan Semanal</h3>
+        <p class="price"><span class="discount"></span> $10/semana</p>
+        <p class="benefit">Acceso por 7 días</p>
+        <ul class="benefits">
+            <li>Compromiso Moderado: Ideal para un enfoque a corto plazo.</li>
+            <li>Sesiones de Empoderamiento: Herramientas para la toma de decisiones informadas sobre el embarazo y el parto.</li>
+            <li>Apoyo Continuo: Acompañamiento constante durante la semana para reducir el estrés y la ansiedad.</li>
+            <li>Mejora del Bienestar: Ejercicios de empoderamiento y bienestar mental.</li>
+        </ul>
+        <button>Elegir plan</button>
+    </div>
+    <div class="plan">
+        <h3>Plan Mensual</h3>
+        <p class="price"><span class="discount"></span> $40/mes</p>
+        <p class="benefit">$50/mes con acompañante, Acceso por 30 días</p>
+        <ul class="benefits">
+            <li>Acceso Ilimitado: Máximo aprovechamiento del servicio durante el embarazo, el parto y el posparto.</li>
+            <li>Reducción de Estrés: Apoyo continuo que promueve una recuperación más rápida y sin complicaciones.</li>
+            <li>Beneficios para la Salud: Menor probabilidad de intervenciones médicas y una experiencia de parto más cómoda.</li>
+        </ul>
+        <button>Elegir plan</button>
+    </div>
+</section>
+
+<div style="height: 40px; background-color: white;"></div>
+
+<?php include '../nwurovida/includes/footer.php'; ?>
+</body>
+</html>

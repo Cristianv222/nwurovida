018d13a527d763b611acd2b63df6fb9f13b4c257
Edison22O <godoyedison465@gmail.com>
2024-11-06 19:29:59 -0500

    contactanos x3
    uwu

--- contactanos.php
+++ contactanos.php
@@ -13,14 +13,18 @@
 <?php include './includes/menu_nav.php'; ?>
 <header class="hero_contac">
     <div class="hero_contac-content container">
-            <div class="hero-txt">
+            <div class="hero_contac-txt">
                 <h1>Contactanos</h1>
-                <a href="">Agenda tu cita</a>
+                <a href="servicios.php" class="btn-Inicio">Agendar Cita</a>
                 </div>
 
     </header>
 
     <div class="contact-container">
+    <section class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1410.4706480803052!2d-77.71259786613068!3d0.816259513493242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e2968bae7d5eb4d%3A0x2d2e73b19f33388d!2zVHVsY8Ohbg!5e0!3m2!1ses!2sec!4v1730240853658!5m2!1ses!2sec" 
+                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
         <section class="contact-info">
             <form action="https://formsubmit.co/1029cristianvasquez@gmail.com" method="POST">
                 <h2>Contacto</h2>
@@ -42,11 +46,6 @@
             </form>
         </section>
     </div>
-    <section class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1410.4706480803052!2d-77.71259786613068!3d0.816259513493242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e2968bae7d5eb4d%3A0x2d2e73b19f33388d!2zVHVsY8Ohbg!5e0!3m2!1ses!2sec!4v1730240853658!5m2!1ses!2sec" 
-                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </section>
     <?php include './includes/footer.php'; ?>
 </body>
-
 </html>

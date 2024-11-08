8c7f76479221d7cc9c2e4f46c4576555ff349732
YYORCH <ingjorge.info@gmail.com>
2024-11-07 20:34:00 -0500

    asdsadad

--- neuro/Servicios_individuales/cursos_de_computacion.php
+++ neuro/Servicios_individuales/cursos_de_computacion.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <header class="header">
-<?php include '../nwurovida/includes/menu_nav.php'; ?>
+<?php include '../../../nwurovida/includes/menu_nav.php'; ?>
     <div class="header-content container">
             <div class="header-txt">
 
